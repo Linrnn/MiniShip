7a78340ce36c6bc9647a8f39a16361440ed3989e
LinRN <44719485+Linrnn@users.noreply.github.com>
2025-11-21 01:12:58 +0800

    chore：【Config】接入多语言（未完成）

--- Tools/Luban/gen.bat
+++ Tools/Luban/gen.bat
@@ -1,9 +1,10 @@
 pushd %~dp0..\..
-@REM set WORKSPACE=..\..
+REM set WORKSPACE=..\..
 set LUBAN_DLL=%cd%\SubModules\luban\src\Luban\bin\Debug\net8.0\Luban.dll
 set LUBAN_CONF=%cd%\Tools\Luban\gen.cfg
 set OUTPUT_DATA=%cd%\Assets\RawResource\Config
 set OUTPUT_CODE=%cd%\Assets\Scripts\Cfg\Gen
+set INPUT_LANG=%cd%\Tools\Luban\lang@__table__.xlsx
 
 dotnet %LUBAN_DLL% ^
     -t all ^
@@ -11,6 +12,11 @@ dotnet %LUBAN_DLL% ^
     -d json ^
     --conf %LUBAN_CONF% ^
     -x outputDataDir=%OUTPUT_DATA% ^
-    -x outputCodeDir=%OUTPUT_CODE%
+    -x outputCodeDir=%OUTPUT_CODE% ^
+    -x l10n.provider=default ^
+    -x l10n.textFile.path=%INPUT_LANG% ^
+    -x l10n.textFile.keyFieldName=key
+    REM -x l10n.textFile.languageFieldName=zh_cn ^
+    REM -x l10n.convertTextKeyToValue=1
 
 pause
---
+++ Tools/Luban/lang.bat
@@ -0,0 +1,15 @@
+pushd %~dp0..\..
+@REM set WORKSPACE=..\..
+set LUBAN_DLL=%cd%\SubModules\luban\src\Luban\bin\Debug\net8.0\Luban.dll
+set LUBAN_CONF=%cd%\Tools\Luban\gen.cfg
+set OUTPUT_LANG=%cd%\Tools\Luban\Output
+
+dotnet %LUBAN_DLL% ^
+    -t all ^
+    -d text-list ^
+    --conf %LUBAN_CONF% ^
+	--validationFailAsError ^
+    -x outputDataDir=%OUTPUT_LANG% ^
+	-x l10n.textListFile=lang.txt
+
+pause
