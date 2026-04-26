9aead812ba143e1202509d5fc3e868e9e6311eb1
LinRN <44719485+Linrnn@users.noreply.github.com>
2026-04-26 14:19:36 +0800

    chore：【插件】升级，【其他】补充图集搜集器，上传ignore

---
+++ Assets/Editor/YooAsset/CollectAtlas.cs
@@ -0,0 +1,17 @@
+﻿using System.IO;
+using YooAsset.Editor;
+
+namespace MiniShipEditor
+{
+    [DisplayName("收集图集")]
+    internal sealed class CollectAtlas : IFilterRule
+    {
+        public string FindAssetType => nameof(EAssetSearchType.All);
+
+        public bool IsCollectAsset(FilterRuleData data)
+        {
+            string extension = Path.GetExtension(data.AssetPath);
+            return extension is ".spriteatlas" or ".spriteatlasv2";
+        }
+    }
+}
